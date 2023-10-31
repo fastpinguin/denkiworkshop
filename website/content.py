ec78097177d08d31075118151051b6f0d6210151
fastpinguin <jokrie04@gmail.com>
2023-10-31 19:04:46 +0100

    save players in tablsave players in tablee

--- website/content.py
+++ website/content.py
@@ -4,6 +4,7 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from random import randint
 from FantasyNameGenerator.Pathfinder import Dhampir 
+from .models import Player
 
 
 content = Blueprint("content", __name__)
@@ -19,17 +20,35 @@ def root():
     else:
         # generate new name if the user does not have one
         new_name = Dhampir.generate()
+        # insert player name into database if it does not exist
+        player = Player.query.filter_by(playerid=new_name).first() # if this returns a user, then it already exists.
+        if player:
+            # try again
+            return redirect(url_for("content.root"))
+        # insert
+        new_player = Player(playerid=new_name, points=0)
+        db.session.add(new_player)
+        db.session.commit()
         resp = make_response(redirect(url_for("content.root")))
         resp.set_cookie("player", new_name, httponly=True)
         return resp
 
 
-@content.route("/api")
+@content.route("/api", methods=["GET"])
 @login_required
 def test():
-    to_return = [randint(0,10) for _ in range(6)]
-    playerID = ["p0", "p1", "p2", "p3", "p4", "p5"]
-    return jsonify(result=dict(zip(playerID,to_return)))
+    players = {}
+    for player in Player.query.all():
+        players[player.playerid] = player.points
+    return jsonify(result=players)
+
+@content.route("/api", methods=["POST"])
+def increase_points():
+    name = request.cookies.get('player')
+    player = Player.query.filter_by(playerid=name).first() # if this returns a user, then it already exists.
+    player.points += 1
+    db.session.commit()
+    return redirect(url_for("content.root"))
 
 
 @content.route("/admin")
